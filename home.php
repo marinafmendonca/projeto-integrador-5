43fd3c3d4eec61fa6ae311a0dca25cb06d21ade3
marinafmendonca <marinafmendonca@hotmail.com>
2018-08-26 20:21:05 -0300

    add footer

--- home.php
+++ home.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/home.css">
     <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
     <title>talkHouse</title>
   </head>
   <body>
@@ -21,7 +22,107 @@
         <a href="cadastro.php" class="btn btn-lg" >Cadastrar</a>
       </div>
     </div>
-  </body>
 
-  <!-- incluir footer -->
+    <div>
+<br>
+<br>
+    </div>
+  </body>
+<!-- incluir footer -->
+<footer class="page-footer font-small indigo">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Central de Ajuda</a>
+          </li>
+          <li>
+            <a href="#!">Termos</a>
+          </li>
+      </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+      <ul class="list-unstyled">
+          <li>
+            <a href="#!">Sobre Nós</a>
+          </li>
+          <li>
+            <a href="#!">Blog</a>
+          </li>
+      </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Trabalhe na talkHouse</a>
+          </li>
+          <li>
+            <a href="#!">Saiba Mais</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mx-auto">
+
+        <!-- Links -->
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Política de Privacidade</a>
+          </li>
+          <li>
+            <a href="#!">Cookies</a>
+          </li>
+      </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:Grupo5
+</div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+
+</footer>
 </html>

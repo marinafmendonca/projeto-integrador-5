78d4dc7fe93246f9d47aebb253bf5c84816e6c7a
fturibiof <fturibiof@gmail.com>
2018-09-10 01:17:50 -0300

    pag usuario

--- usuario.php
+++ usuario.php
@@ -11,26 +11,42 @@
     <script src="js/bootstrap.min.js"></script>
   </head>
   <body>
-    <div class="container" style="width=100px;">
-      <?php
-      if (isset($_FILES['fotoPerfil'])) {
-        $foto = $_FILES['fotoPerfil'];
-        $fotoName = $foto['name'];
-        if ($foto['error'] === UPLOAD_ERR_OK) {
-          $path = "image/".$fotoName;
-          if(file_exists($path)){
-            echo "Arquivo já existe";
-          }
-          else {
-            $ok = move_uploaded_file($foto['tmp_name'], $path);
-            if ($ok) {
-              // echo "Foto ".$foto['name']." salva";
-              echo "<img src='image/$fotoName' alt='foto de perfil'>";
+    <?php require_once 'header.php' ?>
+    <div class="container my-4">
+      <div class="row">
+        <div class="col-lg-4 col-xl-4 text-center" style="border-right:2px solid rgb(200,200,200)">
+          <?php
+          if (isset($_FILES['fotoPerfil'])) {
+            $foto = $_FILES['fotoPerfil'];
+            $fotoName = $foto['name'];
+            if ($foto['error'] === UPLOAD_ERR_OK) {
+              $path = "image/".$fotoName;
+              // if(file_exists($path)){
+              //   echo "Arquivo já existe";
+              // }
+              // else {
+                $ok = move_uploaded_file($foto['tmp_name'], $path);
+                if ($ok) {
+                  echo "<img class='fotoPerfil img-fluid mb-2' src='image/$fotoName' alt='foto de perfil'>";
+                }
+              // }
             }
+          } else {
+            echo "<img class='fotoPerfil img-fluid mb-2' src='image/colearning1.JPG' alt='foto de perfil'>";
           }
-        }
-      }
-      ?>
+          ?>
+          <?php
+            if (isset($_POST['nome'])) {
+              $nome=$_POST['nome'];
+              echo "<h3>$nome</h3>";
+            }
+            else echo "<h3>Nome Sobrenome</h3>";
+           ?>
+          <h4>Campus</h4>
+          <h4>Curso</h4>
+        </div>
+    </div>
   </div>
+  <?php require_once 'footer.php' ?>
   </body>
 </html>

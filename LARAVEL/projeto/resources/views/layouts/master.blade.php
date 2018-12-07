f9ab4e57697a2ca65f103c683bde857654dee8db
LeilaFerreira <leilaferreira.bhte@gmail.com>
2018-12-07 18:28:34 -0200

    Add tema completo

--- LARAVEL/projeto/resources/views/layouts/master.blade.php
+++ LARAVEL/projeto/resources/views/layouts/master.blade.php
@@ -4,40 +4,6 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <title>talkHouse</title>
-  </head>
-  <body>
-    @include('includes.header')
-    <div class="container">
-      @yield('content')
-    </div>
-
-    <!-- <footer id="container-fluid bg-4 text-center">
-      
-        <div style="bottom: 10px; left: 0px; margin-bottom: 6px; position: absolute; text-align: center; width: 100%;">
-        <a href= "#"style="color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Sobre TalkHouse</a>
-        <a href= "#"style="color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Termo de serviço</a>
-        <a href= "#"style="color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Ajuda</a>
-        <a href= "#"style="color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Trabalhe conosco</a>
-      </div> -->
-        
-      <!-- <div class="btn-group" role="group">
-      <button type="button" class= "btn btn-primary pull-right"id="tema-normal"style="background-color: #149AC9; ;color: #fff; margin-bottom: 0"><script>onclick</script>Tema normal</div></button></button>
-      <button type="button" class="btn btn-primary pull-right"id="tema-dark"style="background-color: #149AC9; ;color: #fff; margin-bottom: 0">Novo Tema</div></button></button>
-      </div>
-        </div>
-          </div>
-            </div>
-
-      </footer> -->
-    
-<footer class="container-fluid bg-4 text-center">
-      <a href="#" style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Sobre TalkHouse</a>
-      <a href="#"style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Termo de serviço</a>
-      <a href="#"style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Ajuda</a>
-      <a href="#"style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Trabalhe conosco</a> 
-      
-
-
     <style type="text/css">
     
     .fundo{
@@ -66,9 +32,27 @@
        }
       }
   </script>
+  </head>
+  <body id="corpo" class="fundo">
+    @include('includes.header')
+    <div class="container">
+      @yield('content')
+    </div>
+
+    
+    
+  <footer class="container-fluid bg-4 text-center">
+      <a href="#" style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Sobre TalkHouse</a>
+      <a href="#"style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Termo de serviço</a>
+      <a href="#"style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Ajuda</a>
+      <a href="#"style= "color: #27aae1; font-size: 11px; font-weight: normal; max-width: 135px; padding: 4px 7px;">Trabalhe conosco</a> 
+      
+
+
+    
       <div class="btn-group" role="group">
       <button type="button" class="btn btn-default" onclick="temaNormal();">tema normal</button>
-      <button type="button" class="btn btn-default" onclick="temaDark();">tema dark</button>
+      <button type="button" class="btn btn-default" onclick="temaBlue();">tema Blue</button>
       </div>
     </footer>
 
